034fd6223c4e1f50aa66bb8dcc61bfc673ba5a78
Reejay Salinas <121993503+aizy02@users.noreply.github.com>
2024-02-08 11:57:10 +0800

    Update form.php for socit cloud

--- week8/form.php
+++ week8/form.php
@@ -98,10 +98,16 @@ echo $gender;
 ?>
 
 <?php
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "myDB";
+
+//For SOCIT cloud
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "myDB";
+$username = "webprogmi222_sf221";
+$password = "xE*Y2nleNVvZm[!!";
+$dbname = "webprogmi222_sf221";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
