c0335f956b1488bc24a63597b6ef2db47762f618
Reejay Salinas <121993503+aizy02@users.noreply.github.com>
2024-02-08 12:05:25 +0800

    Update selectdata.php

--- week8/selectdata.php
+++ week8/selectdata.php
@@ -16,7 +16,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, name, email, website,comment,gender FROM MyGuests";
+$sql = "SELECT id, name, email, website,comment,gender FROM rvsalinas_myguests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
